6e352538b66487d1e2a1bf02e806ae4c846149ec
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-04-03 20:51:49 +0800

    servlet 学习完毕

--- learning/11_servlet/3_httpServlet/src/helloRequest/HttpServletDemo.java
+++ learning/11_servlet/3_httpServlet/src/helloRequest/HttpServletDemo.java
@@ -1,4 +1,4 @@
-package httpServletDemo;
+package helloRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -6,21 +6,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-/**
- * 通过不同路径访问同一资源
- */
-@WebServlet({"/m1","/m2","/m3"})
-public class HttpServletMultipathDemo extends HttpServlet {
+@WebServlet("/hs")
+public class HttpServletDemo extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
-        System.out.println("M doGet");
+        System.out.println("HttpServlet doGet");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
-        System.out.println("M doPost");
+        System.out.println("HttpServlet doPost");
     }
 }
--- learning/11_servlet/3_httpServlet/src/helloRequest/HttpServletMultipathDemo.java
+++ learning/11_servlet/3_httpServlet/src/helloRequest/HttpServletMultipathDemo.java
@@ -1,4 +1,4 @@
-package httpServletDemo;
+package helloRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -6,17 +6,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-@WebServlet("/hs")
-public class HttpServletDemo extends HttpServlet {
+
+/**
+ * 通过不同路径访问同一资源
+ */
+@WebServlet({"/m1","/m2","/m3"})
+public class HttpServletMultipathDemo extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
-        System.out.println("HttpServlet doGet");
+        System.out.println("M doGet");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
-        System.out.println("HttpServlet doPost");
+        System.out.println("M doPost");
     }
 }

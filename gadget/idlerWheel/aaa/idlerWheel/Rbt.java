d1a6b4b5446af0bcc0f61f3c9498e86ec68f9fb7
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-05-09 20:20:54 +0800

    做了个鼠标滚轮小工具

---
+++ gadget/idlerWheel/aaa/idlerWheel/Rbt.java
@@ -0,0 +1,21 @@
+package idlerWheel;
+import java.awt.Robot;
+public class Rbt {
+	public static void main(String[] args) {
+		try {
+			Robot robot = new Robot();//创建Robot对象
+			for (int j = 0; j < 1; j++) {//滚动一分钟
+				for (int i = 0; i < 30; i++) {//30 次，共计滚动 30 s
+					robot.mouseWheel(1);//向下滚动 1 下
+					robot.delay(1000);//延时 1000 毫秒
+				}
+				for (int i = 0; i < 30; i++) {//30 次，共计滚动 30 s
+					robot.mouseWheel(-1);//向上滚动 1 下
+					robot.delay(1000);//延时 1000 毫秒
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}

9262a84f7690b665e8f988bfe18e5fad7234a429
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-06-16 20:59:54 +0800

    不记得改了什么，先存个档

--- gadget/fileOperate/src/FileOperate.java
+++ gadget/fileOperate/src/FileOperate.java
@@ -1,12 +1,7 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 
 public class FileOperate {
-    public void copy(File sourceFile,String destinationDir) {
-        //获取源文件的名称
-        String destination = destinationDir + File.separator + sourceFile.getName(); //目标地址
-
+    public void copy(File sourceFile,File destination) {
         try {
             //创建输入输出流对象
             FileInputStream fis = new FileInputStream(sourceFile);
@@ -22,9 +17,36 @@ public class FileOperate {
             //3.释放资源
             fis.close();
             fos.close();
-            System.out.println(sourceFile+"→"+destinationDir);
+            System.out.println(sourceFile.getAbsolutePath()+" →copy→ "+destination.getAbsolutePath());
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void cutFile(File file1, File file2) {
+        FileOutputStream outputStream = null;
+        InputStream inputStream = null;
+        byte[] bytes = new byte[1024];
+        int temp = 0;
+        try {
+            inputStream = new FileInputStream(file1);
+            outputStream = new FileOutputStream(file2);
+            while ((temp = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, temp);
+                outputStream.flush();
+            }
+            System.out.println(file1.getAbsolutePath()+" →cut→ "+file1.getAbsolutePath());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- gadget/fileOperate/src/Main.java
+++ gadget/fileOperate/src/Main.java
@@ -1,14 +1,77 @@
+import saveAndRead.SaveAndRead;
+
 import java.io.File;
-import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 经验：
+ * * 对文件和文件夹命名不能使用以下9个字符：
+ *   / \ : * " < >  ？
+ *   否则会导致 File.mkdir() 失败
+ */
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		GetFile getFile=new GetFile(new File("./"));
-		File dir=new File("百度云可用");
-		dir.mkdir();
-		FileOperate fileOperate = new  FileOperate();
-		for (File file : getFile.getFiles(getFile.selectNameContains("[百度云可用]"))){
-			fileOperate.copy(file,dir.getAbsolutePath());
+        File mayuan = new File("C:\\Users\\86188\\Desktop\\马原\\马原.md");
+        SaveAndRead saveAndRead=new SaveAndRead();
+		String my=saveAndRead.read(mayuan.getAbsolutePath());
+		my=my.replaceAll(" ","").
+				replaceAll("　","").
+				replaceAll("①"," ①").
+				replaceAll("②"," ②").
+				replaceAll("③"," ③").
+				replaceAll("④"," ④").
+				replaceAll("⑤"," ⑤");
+//		System.out.println(my);
+
+		Pattern pTitle = Pattern.compile("\\d{1,3}[.、．].+");
+		//创建Matcher对象
+		Matcher mTitle = pTitle.matcher(my);
+
+		String nextTitle="";
+		String currentTitle="";
+		if (mTitle.find()){
+			currentTitle=mTitle.group(0);
+			while (mTitle.find()){
+				nextTitle=mTitle.group(0);
+
+		        int temp1=my.indexOf(currentTitle);
+		        int temp2=my.indexOf(nextTitle);
+		        String content=my.substring(temp1+currentTitle.length(),temp2);
+		        content=content.
+						replaceAll("A.","\nA.").
+						replaceAll("B.","\nB.").
+						replaceAll("C.","\nC.").
+						replaceAll("D.","\nD.").
+						replaceAll("\n\n\n","\n").
+						replaceAll("\n\n","\n");
+
+//				System.out.println(currentTitle+content);
+
+
+				try{
+					String[] ABCD=new String[4];
+					Pattern p2 = Pattern.compile("[ABCD][.、． ].+");
+					//创建Matcher对象
+					Matcher m2 = p2.matcher(content);
+					int temp=0;
+					while (m2.find()){
+							ABCD[temp++]=m2.group(0);
+					}
+					for (int i=currentTitle.length()-1;i>0;i--){
+						char ans=currentTitle.charAt(i);
+						if ('A'<=ans&&ans<='D'){
+							System.out.println(currentTitle.replaceAll(" ","")
+									+"\n"+ABCD[ans-'A'].replaceAll(" ","")+"\n");
+							break;
+						}
+					}
+				}catch (Exception e){
+//					System.err.println(currentTitle+content);
+				}
+				currentTitle=nextTitle;
+			}
 		}
 	}
 }
